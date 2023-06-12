24f630abfa4bd6c89d40ff10ed04fb7fa000bbe8
RizkyRinaldy <rizky3rinaldy@gmail.com>
2023-06-12 11:25:52 +0800

    Membuat Class MatematikaBeraksi

--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hitung;
 
-/**
- *
- * @author User
- */
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        Matematika rinaldy = new Matematika(2,6);
     
+   System.out.println("Hasil Penjumlahan    ="+rinaldy.setPenjumlahan());
+   System.out.println("Hasil Pengurangan    ="+rinaldy.setPengurangan());
+   System.out.println("Hasil Perkalian   ="+rinaldy.setPerkalian());
+   System.out.println("Hasil Pembagian    ="+rinaldy.setPembagian());
+    }
 }
